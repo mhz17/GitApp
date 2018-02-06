75355506bff2f588acc8ac4b9537de457d67dca5
marta-z <marta.zielinska@oneadvanced.com>
2018-02-06 11:32:19 +0000

    Entities have been updated

---
+++ GitApp/ViewModels/OwnerViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitApp.ViewModels
+{
+    public class OwnerViewModel
+    {
+        [Required]
+        public string login { get; set; }
+        [Required]
+        public int id { get; set; }
+        public string avatar_url { get; set; }
+        public string gravatar_id { get; set; }
+        public string url { get; set; }
+        public string html_url { get; set; }
+        public string followers_url { get; set; }
+        public string following_url { get; set; }
+        public string gists_url { get; set; }
+        public string starred_url { get; set; }
+        public string subscriptions_url { get; set; }
+        public string organizations_url { get; set; }
+        public string repos_url { get; set; }
+        public string events_url { get; set; }
+        public string received_events_url { get; set; }
+        public string type { get; set; }
+        public bool site_admin { get; set; }
+    }
+}
